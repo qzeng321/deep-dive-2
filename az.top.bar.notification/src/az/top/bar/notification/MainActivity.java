8642b75655171a2f6cd26e4dfb415a63817ffcfe
Annie Zeng <aqunzeng@gmail.com>
2012-09-26 15:45:19 -0400

    Fixing bugs found in initial code from 2009 tutorial: still not fully functional

--- az.top.bar.notification/src/az/top/bar/notification/MainActivity.java
+++ az.top.bar.notification/src/az/top/bar/notification/MainActivity.java
@@ -3,19 +3,21 @@ package az.top.bar.notification;
 import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.Notification.Builder;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends Activity
 {
+
     /** Called when the activity is first created. */
-    @Override
+   /** @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
+    } **/
     
     protected void showNotification() {
     // Get NotificationManager service.
@@ -25,27 +27,32 @@ public class MainActivity extends Activity
     String from = this.getString(R.string.app_name);
     String message = this.getString(R.string.message);
         
-    // Create PendingIntent to launch activity if user selects notification.
+ // Create PendingIntent to launch activity if user selects notification.
     PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
             new Intent(this, amiproud.class), 0);
+    
+ 
+// Construct Notification.
+int icon = R.drawable.icon;
+CharSequence tickerText = "Hello";
+long when = System.currentTimeMillis();
 
-    // Construct Notification.
-    Notification notif = new Notification(R.drawable.icon, message,
-            System.currentTimeMillis());
+Notification notification = new Notification(icon, tickerText, when);
 
     // Set info for views that show in notification top panel.
-    notif.setLatestEventInfo(this, from, message, contentIntent);
+    notification.setLatestEventInfo(this, from, message, contentIntent);
 
     // Buzz a bit
     // Make sure your Android Manifest has <uses-permission android:name="android.permission.VIBRATE" />
-    notif.vibrate = new long[] { 200, 500, 100, 250};
+    notification.vibrate = new long[] { 200, 500, 100, 250};
 
     // Note that we use R.string.app_name as the ID for
     // the notification.  It could be any integer you want, but we use
     // the convention of using a resource id for a string related to
     // the notification.  It will always be a unique number within your
     // application.
-    nm.notify(R.string.app_name, notif);
+    nm.notify(R.string.app_name, notification);} 
 }
-    
-}
+
+
+
---
+++ az.top.bar.notification/src/az/top/bar/notification/amiproud.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package az.top.bar.notification;
+
+/**
+ *
+ * @author qzeng
+ */
+public class amiproud {
+    
+}
